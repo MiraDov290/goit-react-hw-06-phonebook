6376df260dc55d2ba48ff24fb9e60a447cfaf0d2
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-09-26 13:01:28 +0100

    changers

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,13 +5,14 @@ import storage from 'redux-persist/lib/storage'
 // Контакти телефона
 const phoneContacts = {
   items: [
-     { id: 'id-1', name: 'Rosie Simpson', number: '369-10-56' },
+   { id: 'id-1', name: 'Rosie Simpson', number: '369-10-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Rostyslav Dovhan', number: '337-81-15' },
       { id: 'id-5', name: 'Myroslava Mazuryk', number: '332-91-11' },
   ],
 };
+
 
 // Створення slice контактів з використанням createSlice
 const contactsSlice = createSlice({
